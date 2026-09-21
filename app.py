4cb7fd5d17dfb434aea0af33a1c83c877785a39a
Sherlock Du <james170120@gmail.com>
2026-09-21 21:01:15 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -108,8 +108,6 @@ if st.button("🚀 開始解析並下載"):
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
             }
         }
-            
-
             try:
                 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                     info = ydl.extract_info(url, download=True)
